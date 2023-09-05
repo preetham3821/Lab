2697122e7b686eaee63e38b07bacb2b4709ae1a1
preetham3821 <21eg106b60@anurag.edu.in>
2023-09-05 14:37:27 +0530

    successfull

---
+++ app_server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
+module.exports = router;
